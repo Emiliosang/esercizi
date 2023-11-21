ec95f4cccaf077602f7078f99982162b58b2e3e9
Emiliosang <104137753+Emiliosang@users.noreply.github.com>
2023-11-21 19:33:28 +0100

    start

---
+++ my-app/src/Login.jsx
@@ -0,0 +1,36 @@
+function Login({ onLogin }) {
+    const [data, setData] = useState({
+        username: '',
+        password: '',
+        session: false,
+    })
+
+    function handleInputChange(event) {
+        const name = event.target.name
+        const value = event.target.value
+        const checked = event.target.checked
+        const type = event.target.type
+
+        setData(d => {
+            return {
+            ...d,
+            [name]: type === "checkbox" ? checked : value,
+            }
+        })
+    }
+
+    function handleLoginClick() {
+        onLogin(data);
+      }
+    return (
+        <div>
+            <input name="username" value={data.username} onChange={handleInputChange} />
+            <input name="password" type="password" value={data.password} onChange={handleInputChange} />
+            <input name="session" type="checkbox" checked={data.session} onChange={handleInputChange} />
+            <label htmlFor="session">Remember me</label>
+            <button disabled={!data.username || !data.password} onClick={handleLoginClick}>Log in</button>
+        </div>
+    )
+}
+
+export default Login
