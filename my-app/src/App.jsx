ddb415062e7e1cc50910a66d0e22e950f69172e7
Emiliosang <104137753+Emiliosang@users.noreply.github.com>
2023-11-21 19:25:25 +0100

    start

---
+++ my-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MouseClicker from './MouseClicker';
+
+function App() {
+  return (
+    <div>
+      <MouseClicker />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ my-app/src/MouseClicker.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function MouseClicker() {
+    const handleClick = ((event) => {
+      console.log(event.target.name);
+    });
+  
+    return (
+      <button name="one" onClick={handleClick}>
+        Click me
+      </button>
+    );
+}
+  
+  export default MouseClicker;
