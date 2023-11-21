72b91c180e47d99a1824bb0de5fc5503f4e00795
Emiliosang <104137753+Emiliosang@users.noreply.github.com>
2023-11-21 19:54:04 +0100

    start

---
+++ my-app/src/TodoList.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const handleInputChange = (event) => {
+    setNewTodo(event.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (newTodo.trim() !== '') {
+      setTodos([...todos, newTodo]);
+      setNewTodo('');
+    }
+  };
+
+  const handleRemoveTodo = (index) => {
+    const updatedTodos = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => handleRemoveTodo(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={handleInputChange}
+        placeholder="Aggiungi un nuovo Todo"
+      />
+      <button onClick={handleAddTodo}>Aggiungi Todo</button>
+    </div>
+  );
+}
+
+export default TodoList;
