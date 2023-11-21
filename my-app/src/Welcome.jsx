59a2337a0d61ee3f081411bf656a9feff2c94003
Emiliosang <104137753+Emiliosang@users.noreply.github.com>
2023-11-21 18:38:38 +0100

    start

---
+++ my-app/src/Welcome.jsx
@@ -0,0 +1,10 @@
+export function Welcome({ name, age }) {
+  return (
+    <div>
+      <p>
+        <strong>{name}</strong>, welcome!
+      </p>
+      <p>Your age is {age}</p>
+    </div>
+  );
+}  
