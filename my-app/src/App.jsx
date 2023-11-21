9f290593eb3a28f95629ee7ea0421f6035c6cffd
Emiliosang <104137753+Emiliosang@users.noreply.github.com>
2023-11-21 18:24:20 +0100

    start

---
+++ my-app/src/App.jsx
@@ -0,0 +1,13 @@
+import { Hello } from "/Hello.jsx";
+import { Message } from "/Message"
+
+export function App(){
+    return (
+        <div>
+          <Hello />
+          <Hello />
+          
+          <Message />
+        </div>
+      );
+    }
