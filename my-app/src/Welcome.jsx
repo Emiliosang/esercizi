29032a7af91f664c2e2573a1e395d26a3f552219
Emiliosang <104137753+Emiliosang@users.noreply.github.com>
2023-11-21 19:57:04 +0100

    start

---
+++ my-app/src/Welcome.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Welcome() {
+    return (
+      <div className="welcome">
+        <h2>Welcome, John!</h2>
+      </div>
+    );
+  }
+  
+  export default Welcome;
---
+++ my-app/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './Index.css'
+import Welcome from './Welcome';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Welcome />
+  </React.StrictMode>,
+)
