a2d40cebb0a6b29751a6f1f9e98d5ae88298e04a
Emiliosang <104137753+Emiliosang@users.noreply.github.com>
2023-12-03 23:44:39 +0100

    start

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Counter from './Counter';
 
 function App() {
   return (
-    <div>
-      <Counter initialCounter={0} incrementAmount={1} />
-      <Counter initialCounter={10} incrementAmount={5} />
-    </div>
+    <Router>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/counter">Counter</Link></li>
+        </ul>
+      <Routes>
+        <Route path="/counter" element={<Counter initialCounter={0} incrementAmount={1} />} />
+        <Route path="/" element={<div>Home Page</div>} />
+        
+      </Routes>
+    </Router>
   );
 }
 
