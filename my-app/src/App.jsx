a568e918d76ba8dd5a03c6cc8ecd5295be145681
Emiliosang <104137753+Emiliosang@users.noreply.github.com>
2023-12-04 01:30:06 +0100

    start

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,6 +2,7 @@ import GithubUser from "./GithubUser"
 import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom"
 import ShowGithubUser from "./ShowGithubUser"
 import MainPage from "./MainPage"
+import Contact from "./Contact"
 
 export default function App() {
   return (
@@ -9,10 +10,14 @@ export default function App() {
     <Link to="/">Home</Link>
     <br />
     <Link to="/users/emiliosang">Show Github User</Link>
+    <br />
+    <Link to="/Contact">Contact us</Link>
     <hr />
     <Routes>
-      <Route path="/users/:username" element={<ShowGithubUser />} />
       <Route path="/" element={<MainPage />} />
+      <Route path="/users/:username" element={<ShowGithubUser />} />
+      <Route path="/Contact" element={<Contact />} />
+
     </Routes>
   </Router>
   )
