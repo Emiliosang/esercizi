a76a32945b99c21b6228b957d1002a20765cb2f8
Emiliosang <104137753+Emiliosang@users.noreply.github.com>
2023-12-16 16:05:43 +0100

    start

--- exercise-2-read-only-arrays-start.ts
+++
@@ -1,9 +0,0 @@
-const languages: ReadonlyArray<string> = ["Mongolian", "French", "Basque", "Thai"];
-
-console.log(languages);
-
-const currencies: readonly string[] = ["NZD", "THB", "NGN", "EUR", "COP", "INR"];
-
-console.log(currencies);
-
-export {};
