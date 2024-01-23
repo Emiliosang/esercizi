0fff410f9fe2fd962675b5572970da118bf2003d
Emiliosang <104137753+Emiliosang@users.noreply.github.com>
2024-01-23 08:09:57 +0100

    START

--- index.mjs
+++
@@ -1,9 +0,0 @@
-import { writeFile } from 'node:fs';
-
-writeFile('message.txt', 'Hello Node.js', 'utf8', function(err, data) {
-    if(err){
-        console.error(err);
-        return;
-    }
-    console.log("operazione riuscita")
-});
